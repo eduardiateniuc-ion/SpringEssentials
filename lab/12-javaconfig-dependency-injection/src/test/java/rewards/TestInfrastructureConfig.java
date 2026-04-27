14265e968d807d6e5d91db7116c5ef91dc644be9
Eduard Iateniuc <crme335@cedmd.ced.it>
2026-04-27 18:26:26 +0300

    first commit

--- lab/12-javaconfig-dependency-injection/src/test/java/rewards/TestInfrastructureConfig.java
+++ lab/12-javaconfig-dependency-injection/src/test/java/rewards/TestInfrastructureConfig.java
@@ -1,7 +1,9 @@
 package rewards;
 
+import config.RewardsConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 
 import javax.sql.DataSource;
@@ -53,12 +55,9 @@ import javax.sql.DataSource;
  *
  */
 @Configuration
+@Import(RewardsConfig.class)
 public class TestInfrastructureConfig {
 
-	/**
-	 * Creates an in-memory "rewards" database populated
-	 * with test data for fast testing
-	 */
 	@Bean
 	public DataSource dataSource() {
 		return (new EmbeddedDatabaseBuilder()) //
